81a940172748f4005d4176731a68150224fb6a18
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-07 12:19:55 -0700

    add simple js functions to address-book

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
